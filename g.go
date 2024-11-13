dfa245366af1f0a81b8bf7d201b2bbbb3156ac74
Ivan Georgiev <ivan.georgiev4360@gmail.com>
2024-11-13 19:03:54 +0200

    Use a CLI library

--- g.go
+++ g.go
@@ -3,12 +3,14 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"log"
 	"os"
 	"sfcc/g/kv"
 	"sfcc/g/sfcc"
 	"sfcc/g/util"
 
 	env "github.com/joho/godotenv"
+	"github.com/urfave/cli/v2"
 )
 
 var apiID string
@@ -44,22 +46,39 @@ func main() {
 	kv.Init(constantPath)
 	defer kv.Close()
 
-	sfcc.GetSfccAuthToken(apiID, apiSecret)
-	sandboxList := sfcc.GetSandboxList(true)
+	app := &cli.App{
+		Name:  "sfcc-g",
+		Usage: "Perform sfcc actions with a Go CLI",
+		Commands: []*cli.Command{
+			{
+				Name:  "list",
+				Usage: "List fresh data for all sandboxes",
+				Action: func(cCtx *cli.Context) error {
+					sfcc.GetSfccAuthToken(apiID, apiSecret)
+					sandboxList := sfcc.GetSandboxList(true)
 
-	var summary bytes.Buffer
-	for _, sb := range sandboxList {
-		var stateEmoji string
-		if sb.State == "started" {
-			stateEmoji = "🟢"
-		} else if sb.State == "stopped" {
-			stateEmoji = "🔴"
-		} else {
-			stateEmoji = "🟡"
-		}
+					var summary bytes.Buffer
+					for _, sb := range sandboxList {
+						var stateEmoji string
+						if sb.State == "started" {
+							stateEmoji = "🟢"
+						} else if sb.State == "stopped" {
+							stateEmoji = "🔴"
+						} else {
+							stateEmoji = "🟡"
+						}
 
-		summary.WriteString(fmt.Sprintf("%s🔹%s%s%s\n", sb.ID, sb.HostName, stateEmoji, sb.State))
+						summary.WriteString(fmt.Sprintf("%s🔹%s%s%s\n", sb.ID, sb.HostName, stateEmoji, sb.State))
+					}
+
+					fmt.Println(summary.String())
+					return nil
+				},
+			},
+		},
 	}
 
-	fmt.Println(summary.String())
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
